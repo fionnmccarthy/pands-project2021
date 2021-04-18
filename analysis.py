9802b386f9c917168c8f522908f2daee1a4396fd
Fionn McCarthy <G00301126@gmit.ie>
2021-04-18 21:06:23 +0100

    Summarizing the iris flower data

--- analysis.py
+++ analysis.py
@@ -7,7 +7,10 @@ import numpy as np # wused for working with arrays
 import pandas as pd # used for importing data for data analysis
 
 irisdata = pd.read_csv('iris_data.csv')
+pd.set_option("display.precision",2) # set figures so be returned to two decimal places
+iris_summary = irisdata.describe()
+print(iris_summary)
 
-print(irisdata)
+#print(irisdata)
 
 # https://datahub.io/machine-learning/iris data set source on 17/04/2021
